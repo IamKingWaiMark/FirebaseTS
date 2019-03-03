47e37912ed8cf84b83e753cb48672ed5b9ed2b82
King <kingwaimark@gmail.com>
2019-03-03 03:39:09 -0500

    V 0.0.12

--- firebasetsFirestore/firebaseTSFirestore.d.ts
+++ firebasetsFirestore/firebaseTSFirestore.d.ts
@@ -1,4 +1,19 @@
 import { } from 'firebase/app';
+
+
 export declare class FirebaseTSFirestore {
-    listenTo(from: string [], onUpdate: (result: firebase.firestore.QuerySnapshot) => void): any;
+    public listenTo(from: string [], where: Where[] | OrderBy [] | Limit [], onUpdate: (result: firebase.firestore.QuerySnapshot) => void): any;
+}
+
+
+export declare class Where {
+    constructor(where: string, condition: "<=" | "<" | "==" | ">" | ">=", argument: any);
+}
+
+export class OrderBy {
+    constructor(field: string, direction: "asc" | "desc");
+}
+
+export class Limit {
+    constructor(limit: number);
 }
--- firebasetsFirestore/firebaseTSFirestore.js
+++ firebasetsFirestore/firebaseTSFirestore.js
@@ -3,14 +3,30 @@ import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
 require("firebase/firestore");
 
 export class FirebaseTSFirestore {
-    listenTo(from, onUpdate){
-        const fromArr = Array(from);
+
+    listenTo(from, where, onUpdate){
+        let cr = this.genCollectionReference(from);
+        
+        if((where != null || where != undefined) && where.length > 0) {
+            let query = undefined;
+            const whereArr = Array(where)[0];
+            for(let i = 0; i < whereArr.length; i++){
+                if(query === undefined) query = whereArr[i].getQuery(cr);
+                else query = whereArr[i].concatQuery(query);
+            }
+
+            return query.onSnapshot(results => onUpdate(results));
+        } 
+        return cr.onSnapshot(results => onUpdate(results));
+    }
+    genCollectionReference(from){
+        const fromArr = Array(from)[0];
         let cr = undefined;
         let dr = undefined;
-        for(let i = 0; i < fromArr[0].length; i++){
+        for(let i = 0; i < fromArr.length; i++){
             if(i % 2 == 0){
                 if(cr === undefined){
-                    cr = FirebaseTSApp.getFirestore().collection(fromArr[0][i]);
+                    cr = FirebaseTSApp.getFirestore().collection(fromArr[i]);
                 } else {
                     cr = dr.collection(fromArr[i]);
                 }
@@ -18,7 +34,53 @@ export class FirebaseTSFirestore {
                 dr = cr.doc(fromArr[i]);
             }
         }
-        
-        return cr.onSnapshot(results => onUpdate(results));
+
+        return cr;
+    }
+
+}
+
+
+export class Where {
+
+    constructor(where, condition, argument){
+        this.where = where;
+        this.condition = condition;
+        this.argument = argument;
+    }
+
+    getQuery(cr){
+        return cr.where(this.where, this.condition, this.argument);
+    }
+    concatQuery(query){
+        return query.where(this.where, this.condition, this.argument);
+    }
+}
+
+export class OrderBy {
+    constructor(field, direction){
+        this.field = field;
+        this.direction = direction;
+    }
+
+    getQuery(cr){
+        return cr.orderBy(this.field, this.direction);
+    }
+    concatQuery(query){
+        return query.orderBy(this.field, this.direction);
+    }
+
+}
+
+export class Limit {
+    constructor(limit){
+        this.limit = limit;
+    }
+
+    getQuery(cr){
+        return cr.limit(this.limit);
+    }
+    concatQuery(query){
+        return query.limit(this.limit);
     }
 }
