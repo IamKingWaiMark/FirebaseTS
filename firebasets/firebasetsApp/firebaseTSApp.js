9d91f4203d18b2d2532b60552279fffd8a97d9db
King <kingwaimark@gmail.com>
2019-03-02 20:41:29 -0500

    0.0.4

--- firebasets/firebasetsApp/firebaseTSApp.js
+++
@@ -1,5 +0,0 @@
-export class FirebaseTSApp {
-    static init(){
-        return [console.log("BETA SUCCESSFUL")];
-    }
-}
---
+++ firebasetsApp/firebaseTSApp.js
@@ -0,0 +1,7 @@
+
+export class FirebaseTSApp {
+    static init(){
+        console.log("BETA SUCCESSFUL");
+        return [];
+    }
+}
